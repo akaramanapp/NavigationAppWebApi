e01a7b6b852423f929371058cdb3830c63cde5cc
Abdulkerim Karaman <abdulkerimkaraman@gmail.com>
2022-01-16 01:28:55 +0300

    Proje dosyası ekle.

---
+++ NavigationAppWebApi/Controllers/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NavigationAppWebApi.Dto;
+using System.Collections.Generic;
+
+namespace NavigationAppWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : Controller
+    {
+        [HttpGet]
+        public List<MenuItem> GetMenuItems()
+        {
+            List<MenuItem> menuList = new List<MenuItem>();
+            MenuItem menuItem1 = new MenuItem
+            {
+                Name = "About",
+                Path = "about"
+            };
+
+            MenuItem menuItem2 = new MenuItem
+            {
+                Name = "Dashboard",
+                Path = "dashboard"
+            };
+
+            menuList.Add(menuItem1);
+            menuList.Add(menuItem2);
+
+            return menuList;
+        }
+    }
+}
---
+++ NavigationAppWebApi/Dto/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NavigationAppWebApi.Dto
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+    }
+}
